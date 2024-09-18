6bc1c7985f0fa46e170696b7183d91c1aeb55d87
zackees <z@zackees.com>
2024-09-18 01:26:50 -0700

    Add configure_led function for LED strip initialization

--- src/led_strip/demo.cpp
+++ src/led_strip/demo.cpp
@@ -3,6 +3,7 @@
 #include "led_strip/led_strip.h"
 #include "demo.h"
 #include "esp_log.h"
+#include "configure_led.h"
 
 #include "namespace.h"
 LED_STRIP_NAMESPACE_BEGIN
@@ -37,51 +38,7 @@ bool is_rgbw_mode_active(led_pixel_format_t rgbw_mode) {
     return rgbw_mode == LED_PIXEL_FORMAT_GRBW;
 }
 
-
-led_strip_handle_t configure_led(int pin, uint32_t max_leds, led_model_t chipset, led_pixel_format_t rgbw) {
-    // LED strip general initialization, according to your led board design
-    led_strip_config_t strip_config = {};
-    strip_config.strip_gpio_num = pin;
-    strip_config.max_leds = max_leds;
-    strip_config.led_pixel_format = rgbw;
-    strip_config.led_model = chipset;
-    strip_config.flags.invert_out = 0;
-
-    // print out the values of the configuration
-    ESP_LOGI(TAG, "strip_config.strip_gpio_num: %d",
-             strip_config.strip_gpio_num);
-    ESP_LOGI(TAG, "strip_config.max_leds: %d", strip_config.max_leds);
-    ESP_LOGI(TAG, "strip_config.led_pixel_format: %d",
-             strip_config.led_pixel_format);
-    ESP_LOGI(TAG, "strip_config.led_model: %d", strip_config.led_model);
-    ESP_LOGI(TAG, "strip_config.flags.invert_out: %d",
-             strip_config.flags.invert_out);
-
-    led_strip_rmt_config_t rmt_config = {};
-    memset(&rmt_config, 0, sizeof(led_strip_rmt_config_t));
-    rmt_config.clk_src = RMT_CLK_SRC_DEFAULT;
-    rmt_config.resolution_hz = 10000000;
-    rmt_config.mem_block_symbols = 64;
-    rmt_config.flags.with_dma = false;
-
-    ESP_LOGI(TAG, "rmt_config.clk_src: %d", rmt_config.clk_src);
-    ESP_LOGI(TAG, "rmt_config.resolution_hz: %d", rmt_config.resolution_hz);
-    ESP_LOGI(TAG, "rmt_config.mem_block_symbols: %d",
-             rmt_config.mem_block_symbols);
-    ESP_LOGI(TAG, "rmt_config.flags.with_dma: %d", rmt_config.flags.with_dma);
-
-    // LED Strip object handle
-    led_strip_handle_t led_strip;
-    ESP_ERROR_CHECK(
-        led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-    ESP_LOGI(TAG, "Created LED strip object with RMT backend");
-    return led_strip;
-}
-
-
-
-
-void run_led_demo(led_strip_handle_t led_strip, uint32_t num_leds, bool rgbw_active) {
+void draw_loop(led_strip_handle_t led_strip, uint32_t num_leds, bool rgbw_active) {
     const int MAX_BRIGHTNESS = 5;
     bool led_on_off = false;
     while (1) {
@@ -120,7 +77,7 @@ void demo(int led_strip_gpio, uint32_t num_leds, LedStripMode mode) {
     to_esp_modes(mode, &chipset, &rgbw_mode);
     const bool is_rgbw_active = is_rgbw_mode_active(rgbw_mode);
     led_strip_handle_t led_strip = configure_led(led_strip_gpio, num_leds, chipset, rgbw_mode);
-    run_led_demo(led_strip, num_leds, is_rgbw_active);
+    draw_loop(led_strip, num_leds, is_rgbw_active);
 }
 
 
