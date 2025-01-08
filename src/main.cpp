57a15febc85f2bafe9efdffc58671378071a4672
zackees <z@zackees.com>
2025-01-08 13:53:52 -0800

    feat: add SPI support for LED strip control

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
+
+#define USE_SPI
+
+#ifdef USE_SPI
+#include "../examples/led_strip_spi_ws2812/main/led_strip_spi_ws2812_main.c"
+#else
 #include "../examples/led_strip_rmt_ws2812/main/led_strip_rmt_ws2812_main.c"
+#endif
 
 void setup() {}
 void loop() {
